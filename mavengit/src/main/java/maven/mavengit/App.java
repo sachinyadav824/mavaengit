4508301fe292b07e382788fa9f4a9be4981da59d
sachin <sachin.softeng@gmail.com>
2018-04-15 17:13:28 +0530

    done

---
+++ mavengit/src/main/java/maven/mavengit/App.java
@@ -0,0 +1,13 @@
+package maven.mavengit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
